951853e8ddfae64a4784736c6ec24c19327873d2
Benjamin Jaume <47286801+BenjaminJaume@users.noreply.github.com>
2020-03-11 14:14:33 -0600

    Minor fix

--- src/components/GoogleCalendar/fetch.js
+++ src/components/GoogleCalendar/fetch.js
@@ -24,57 +24,58 @@ export function getEvents(callback) {
       let events = [];
 
       JSON.parse(resp.text).items.map(event => {
-        if (event.status === "confirmed") {
-          // If the event is recurrent
-          if (event.recurrence) {
-            var tempArray = [];
-            var rule;
+        // If the event is recurrent
+        if (event.recurrence) {
+          var tempArray = [];
+          var rule;
 
-            // If a date until the event occurs is provided
-            if (event.recurrence[0].indexOf("UNTIL=") !== -1) {
-              const data = extractDate(event.recurrence[0]);
-              const until = new Date(data.year, data.month - 1, data.day);
+          // If a date until the event occurs is provided
+          if (event.recurrence[0].indexOf("UNTIL=") !== -1) {
+            const data = extractDate(event.recurrence[0]);
+            const until = new Date(data.year, data.month - 1, data.day);
 
-              // Create an array of events
-              rule = new RRule({
-                freq: RRule.WEEKLY,
-                dtstart: new Date(event.start.dateTime),
-                until
-              });
-            } else {
-              // If NO date until the event occurs is NOT provided
-              // Create an array of events until the end of the year of the event
-              rule = new RRule({
-                freq: RRule.WEEKLY,
-                dtstart: new Date(event.start.dateTime),
-                until: new Date(
-                  event.start.dateTime.toString().slice(0, 4),
-                  11,
-                  31
-                )
-              });
-            }
-
-            rule.all();
-
-            // Calculate duration (in hours)
-            var duration =
-              (new Date(event.end.dateTime) - new Date(event.start.dateTime)) /
-              3600000;
-
-            rule._cache.all.forEach(item => {
-              const endTime = new Date(item);
-              endTime.setHours(item.getHours() + duration);
-
-              tempArray.push({
-                start: item,
-                end: endTime,
-                title: event.summary
-              });
+            // Create an array of events
+            rule = new RRule({
+              freq: RRule.WEEKLY,
+              dtstart: new Date(event.start.dateTime),
+              until
             });
-
-            events = events.concat(tempArray);
           } else {
+            // If the date until the event occurs is NOT provided
+            // Create an array of events until the end of the year of the event
+            rule = new RRule({
+              freq: RRule.WEEKLY,
+              dtstart: new Date(event.start.dateTime),
+              until: new Date(
+                event.start.dateTime.toString().slice(0, 4),
+                11,
+                31
+              )
+            });
+          }
+
+          rule.all();
+
+          // Calculate duration (in hours)
+          var duration =
+            (new Date(event.end.dateTime) - new Date(event.start.dateTime)) /
+            3600000;
+
+          rule._cache.all.forEach(item => {
+            const endTime = new Date(item);
+            endTime.setHours(item.getHours() + duration);
+
+            tempArray.push({
+              start: item,
+              end: endTime,
+              title: event.summary
+            });
+          });
+
+          events = events.concat(tempArray);
+        } else {
+          console.log(!event.recurringEventId);
+          if (event.status === "confirmed" && !event.recurringEventId) {
             events.push({
               start: new Date(event.start.dateTime),
               end: new Date(event.end.dateTime),
